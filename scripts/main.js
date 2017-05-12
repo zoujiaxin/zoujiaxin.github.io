f5190b1278d026b8f06f5f444e28cf2606b1878c
zoujiaxin <xinjiazou@163.com>
2017-05-12 11:40:47 +0800

    js
    js

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/f.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.'); //而且在用户点击 OK 后将数据存储在变量里
+  localStorage.setItem('name', myName); //允许我们将数据存储在浏览器里以供后续调用
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+//允许用户设置新名字
+myButton.onclick = function() {
+  setUserName();
+}
